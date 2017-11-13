082ba74e003e5d433b5b7fd5115ba6e81c315f7f
Kyle Nielsen <nielsen.kyle93@utexas.edu>
2017-11-13 15:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments) {
   var musicPairs = []
-  for (let i = 0, i < 4, i++) {
+  for (let i = 0; i < 4; i++) {
     musicPairs.push("${musicians} plays ${instruments}")
   }
   return musicPairs
